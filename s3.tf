3e559d1b553e085cb9d9d962a22a658351ce5554
Diego Moraes <contato@comunidadetilabs.com.br>
2025-02-12 22:30:29 -0300

    Remove ACL setting from S3 bucket module for improved security practices

--- s3.tf
+++ s3.tf
@@ -8,7 +8,6 @@ module "s3_bucket" {
     source  = "terraform-aws-modules/s3-bucket/aws"
 
     bucket = var.bucket_name
-    acl    = "private"
 
     tags = {
         Name        = var.bucket_name
